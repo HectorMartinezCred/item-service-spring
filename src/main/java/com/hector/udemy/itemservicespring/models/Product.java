b294b66c6c77ba6f71f17a20d30589387aa06c13
HectorMartinezCred <hector.martinez.cred@outlook.com>
2022-10-19 16:58:10 -0500

    Create Product.java

---
+++ src/main/java/com/hector/udemy/itemservicespring/models/Product.java
@@ -0,0 +1,17 @@
+package com.hector.udemy.itemservicespring.models;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private Date createDate;
+}
